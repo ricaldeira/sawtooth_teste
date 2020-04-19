11e8250ed360eb9ec2d07362d5e35a8bc9c6868e
Ricardo Caldeira <ricardocaldeira@gmail.com>
2020-04-19 11:38:58 -0300

    iniciando meus documentos

--- frontend/pages/documents/index.vue
+++ frontend/pages/documents/index.vue
@@ -1,3 +1,28 @@
 <template>
-    <div>Docuemnts</div>
+    <div class="section">
+        <input type="file" @change="onFileChanged">
+        <button @click="onUpload"> Upload </button>
+        
+
+    </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+            selectedFile: null
+        }
+    },
+    methods:{
+        onFileChanged(event){
+            const file = event.target.files[0]
+        },
+        onUpload(){
+            //retunr hash file from rest-api
+            //axios.post('localhost/file_upload', this.selectedFile)
+
+        }
+    }
+}
+</script>
